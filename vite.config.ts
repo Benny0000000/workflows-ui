2f4132730998548d14a34f3a1681b792d89d0648
Bennett <bennybenester@gmail.com>
2026-01-10 15:52:39 +0300

    Add conf for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    host: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost",
+        changeOrigin: true,
+        configure: (_proxy, _options) => {
+          _options;
+          _proxy;
+        },
+      },
+    },
+  },
+});
