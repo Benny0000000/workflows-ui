226b9acf618985bfee32081b34c6fb144ea2b349
Benny <bennyivan14@gmail.com>
2026-01-17 03:29:51 +0300

    Complete Login and SignUp UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    host: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost",
-        changeOrigin: true,
-        configure: (_proxy, _options) => {
-          _options;
-          _proxy;
-        },
-      },
-    },
-  },
-});
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
